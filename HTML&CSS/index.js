e693fc64b711685723f9ffd7c9f6c92c9e2182f7
JeongSooNa <skwjdtn3171@naver.com>
2022-06-23 17:09:40 +0900

    feat js create구현완료

--- HTML&CSS/index.js
+++ HTML&CSS/index.js
@@ -9,12 +9,66 @@ $(".pw-or-not-n").on("click", function () {
 
 // 고객센터문의 글쓰기 등록 눌렀을 때
 $(".service-write-registration").on("click", function () {
-  var password = $(".service-write-pw").val();
-  var rePassword = $(".service-write-re-pw").val();
-  if (password != rePassword) {
-    alert("비밀번호를 확인 해 주세요");
+  // 빈칸 확인
+  if ($('#service-category option:selected').text() == "문의 유형을 선택해 주세요"){
+    alert("문의 유형을 선택해 주세요");
+    return false;
+  }
+  if ($('#writerValue').val() == ""){
+    alert("작성자를 입력하세요");
+    $('#writerValue').focus();
+    return false;
+  }
+  if ($('#emailValue').val() == ""){
+    alert("e-mail을 입력하세요"); // email 형식에 맞게 확인하는 로직구현 필요
+    $('#emailValue').focus();
+    return false;
+  }
+  if ($('#textValue').val() == ""){
+    alert("본문을 입력하세요");
+    $('#textValue').focus();
+    return false;
+  }
+  if ($('.pw-or-not-y').is(":checked")){
+    // password 확인
+    var password = $(".service-write-pw").val();
+    var rePassword = $(".service-write-re-pw").val();
+    if (password != rePassword) {
+      alert("비밀번호를 확인 해 주세요");
+      return false;
+    }
   }
   // post ajax구현
+  var boardCategory = $('#service-category option:selected').text();
+  var boardWriter = $('#writerValue').val();
+  var boardEmail = $('#emailValue').val();
+  var boardContent = $('#textValue').val();
+  var boardPassword = $(".service-write-pw").val();
+  var isBoardSecurity = $('.pw-or-not-y').is(":checked");
+
+  var jsonData = {
+      boardCategory : boardCategory,
+      boardWriter : boardWriter,
+      boardEmail : boardEmail,
+      boardContent : boardContent,
+      boardPassword : boardPassword,
+      isBoardSecurity : isBoardSecurity
+  }
+  //ajax
+  $.ajax({
+    url : 'http://localhost:8080/board',
+    type : 'POST',
+    contentType : 'application/json', // 서버에 json type으로 보낼 예정(요청)
+    dataType : 'json', // 서버 결과를 json으로 응답 받겠다.
+    data : JSON.stringify(jsonData),
+    success : function(response){
+        // 전송 한 data가 DB에 저장되었으면 alert
+        if(response>0){
+          alert("저장이 완료되었습니다.")
+        }
+        console.log(response);
+    }
+})
 });
 
 // 관리자 로그인
